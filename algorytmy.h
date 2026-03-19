04c68cfcba7090eec03463db64a3de341b35c815
Mateusz <01206777@pw.edu.pl>
2026-03-19 19:24:57 +0100

    Aktualizacja

--- algorytmy.h
+++ algorytmy.h
@@ -3,7 +3,7 @@
 
 #include "STRUCTWie.h"
 
-void uklad_fr(grafWieCal *wierzcholki, grafKraCal *krawedzie, int iteracje, double szer, double wys);
+void uklad_fre(grafWieCal *wierzcholki, grafKraCal *krawedzie, int iteracje, double szer, double wys);
 void uklad_tut(grafWieCal *wierzcholki, grafKraCal *krawedzie, int iteracje);
 void uklad_spe(grafWieCal *wierzcholki, grafKraCal *krawedzie, int iteracje);
 
